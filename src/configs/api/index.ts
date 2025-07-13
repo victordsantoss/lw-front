0bba992ec412ea89d0a036de61ee021517c65dd9
Victor - Trabalho <victor.samuelsantoss@gmail.com>
2025-07-13 20:02:18 -0300

    chore: add api config

---
+++ src/configs/api/index.ts
@@ -0,0 +1,23 @@
+import { AuthStorage } from '@/storages/local/auth.storage';
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+  timeout: 10000,
+});
+api.interceptors.request.use(
+  config => {
+    const token = AuthStorage.getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
